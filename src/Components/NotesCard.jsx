e203590e636da499c802336cb94953d50ab81fb7
drilya989 <matylda.glazik@gmail.com>
2024-06-16 23:27:31 +0200

    widok pojedynczej notatki

--- src/Components/NotesCard.jsx
+++ src/Components/NotesCard.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Link} from 'react-router-dom';
+import React, { useState } from 'react';
 import './NotesCard.scss';
+import NotesPopup from './NotesPopup';
 
-const NotesCard = () => {
+const NotesCard = ({ note }) => {
+  const [popupVisible, setPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setPopupVisible(!popupVisible); // Odwróć stan widoczności popupa
+  };
+
   return (
-    //trzeba dodać link potem
-        <div className='card'>
-            <div className="top-of-card">
-                <p id="date">16.06.2024</p>
-              {/*  <p id="username">@Paweł</p> */}
-            </div>
-            <hr noshade="true"/>
-            <div className='bottom-of-card'>
-                <p id="title">Notatki z chemii</p>
-                <p id="notebody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation.  Ut enim ad minim veniam, quis nostrud exercitation 
-                </p>
-                
-            </div>
-        </div>
- 
-  )
-}
+    <div className='card' onClick={togglePopup}> {/* Obsługa kliknięcia na dowolnym miejscu w karcie */}
+      <div className="top-of-card">
+        <p id="date">16.06.2024</p>
+      </div>
+      <hr />
+      <div className='bottom-of-card'>
+        <p id="title">Notatki z chemii</p>
+        <p id="notebody">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation.  Ut enim ad minim veniam, quis nostrud exercitation 
+        </p>
+      </div>
+      
+      {popupVisible && <NotesPopup note={note} onClose={() => setPopupVisible(false)} />} {/* Wyświetl popup gdy popupVisible jest true */}
+    </div>
+  );
+};
 
-export default NotesCard
+export default NotesCard;
 
